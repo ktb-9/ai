55ed631151ce62a714c4b9fe30c7b9734e742044
kwangyoulsagong <sgky0511@naver.com>
2024-12-19 11:21:32 +0900

    feat: change cors

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ app = FastAPI(title="Image Editor API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://client.zero-dang.com/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
